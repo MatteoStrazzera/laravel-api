845088c367f4f440fd2e4485ccfb19a02712f4e6
MatteoStrazzera <mattestrazz@gmail.com>
2024-05-31 17:03:29 +0200

    Add ProjectController in API folder and route get

---
+++ app/Http/Controllers/API/ProjectController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::with('type', 'technologies')->paginate(6);
+        return response()->json([
+            'success' => true,
+            'projects' => $projects
+        ]);
+    }
+}
